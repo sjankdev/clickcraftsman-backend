ce745e78ad1a18fb12bb76e41e7c6134490f4347
stefanjanko <stefanjanko.dev@gmail.com>
2023-12-11 18:38:24 +0100

    Implemented JWT token and security.

--- src/main/java/com/clickcraft/demo/ClickCraftsmanApplication.java
+++ src/main/java/com/clickcraft/demo/ClickCraftsmanApplication.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.clickcraft.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})public class ClickCraftsmanApplication {
+@SpringBootApplication
+public class ClickCraftsmanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClickCraftsmanApplication.class, args);
---
+++ src/main/java/com/clickcraft/demo/repository/UserRepository.java
@@ -0,0 +1,17 @@
+package com.clickcraft.demo.repository;
+
+import java.util.Optional;
+
+import com.clickcraft.demo.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  Optional<User> findByUsername(String username);
+
+  Boolean existsByUsername(String username);
+
+  Boolean existsByEmail(String email);
+}
