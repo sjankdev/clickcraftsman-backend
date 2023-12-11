c14d4954375491f4daa699dabe6fb8865659ceba
stefanjanko <stefanjanko.dev@gmail.com>
2023-12-11 17:35:45 +0100

    initial setup for database

--- src/main/java/com/example/demo/ClickCraftsmanApplication.java
+++ src/main/java/com/example/demo/ClickCraftsmanApplication.java
@@ -2,9 +2,9 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
-public class ClickCraftsmanApplication {
+@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})public class ClickCraftsmanApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ClickCraftsmanApplication.class, args);
