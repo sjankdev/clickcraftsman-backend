8613f561ad3961650628cfcf53f5f8085af5500f
stefanjanko <stefanjanko.dev@gmail.com>
2024-01-10 17:48:29 +0100

    Added @ validations on registration fields

--- src/main/java/com/clickcraft/demo/payload/request/SignupRequest.java
+++ src/main/java/com/clickcraft/demo/payload/request/SignupRequest.java
@@ -21,12 +21,20 @@ public class SignupRequest {
 
     private Set<String> role;
 
+    @NotBlank
+    @Size(min = 3, max = 20)
     private String firstName;
 
+    @NotBlank
+    @Size(min = 3, max = 20)
     private String lastName;
 
+    @NotBlank
+    @Size(min = 3, max = 20)
     private String contactPhone;
 
+    @NotBlank
+    @Size(min = 2, max = 40)
     private String location;
 
     private String skills;
