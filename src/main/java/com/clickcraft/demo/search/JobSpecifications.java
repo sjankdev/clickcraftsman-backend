9968169ea8d78511c278090f469d20ab3ca10d74
Stefan <stefanjanko.dev@gmail.com>
2024-05-14 13:28:44 +0200

    Minor refactoring in job specifications

--- src/main/java/com/clickcraft/demo/search/JobSpecifications.java
+++ src/main/java/com/clickcraft/demo/search/JobSpecifications.java
@@ -13,16 +13,24 @@ import java.util.stream.Stream;
 
 public interface JobSpecifications {
 
+    static Specification<ClientJobPosting> hasLocations(List<ELocations> locations) {
+        return (root, query, criteriaBuilder) -> root.get("location").in(locations);
+    }
+
     static Specification<ClientJobPosting> hasRequiredSkillIds(List<Long> filters) {
-        return (root, query, criteriaBuilder) -> root.join("filters").get("id").in(filters);
+        return (root, query, criteriaBuilder) -> root.join("requiredSkills").get("id").in(filters);
     }
 
     static Specification<ClientJobPosting> buildSpecification(Map<String, String> params) {
         return (root, query, criteriaBuilder) -> {
             List<Predicate> predicates = new ArrayList<>();
-            if (params.containsKey("filterId")) {
-                List<Long> filterId = parseLongList(params.get("filterId"));
-                predicates.add(hasRequiredSkillIds(filterId).toPredicate(root, query, criteriaBuilder));
+            if (params.containsKey("requiredSkillsId")) {
+                List<Long> requiredSkillsId = parseLongList(params.get("requiredSkillsId"));
+                predicates.add(hasRequiredSkillIds(requiredSkillsId).toPredicate(root, query, criteriaBuilder));
+            }
+            if (params.containsKey("locations")) {
+                List<ELocations> locations = parseLocationList(params.get("locations"));
+                predicates.add(hasLocations(locations).toPredicate(root, query, criteriaBuilder));
             }
             return criteriaBuilder.and(predicates.toArray(new Predicate[0]));
         };
@@ -30,5 +38,9 @@ public interface JobSpecifications {
 
     static List<Long> parseLongList(String input) {
         return Stream.of(input.split(",")).map(Long::valueOf).toList();
+    }
+
+    static List<ELocations> parseLocationList(String input) {
+        return Stream.of(input.split(",")).map(ELocations::valueOf).toList();
     }
 }
