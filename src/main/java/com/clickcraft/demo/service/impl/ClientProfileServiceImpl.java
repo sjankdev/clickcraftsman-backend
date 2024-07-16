4d66f8b2e4531485b96075503c8be09aa03fcf40
sjankdev <stefanjanko.dev@gmail.com>
2024-07-16 17:26:04 +0200

    Implement ClientProfileConverter for updating client profile details

--- src/main/java/com/clickcraft/demo/service/impl/ClientProfileServiceImpl.java
+++ src/main/java/com/clickcraft/demo/service/impl/ClientProfileServiceImpl.java
@@ -3,10 +3,10 @@ package com.clickcraft.demo.service.impl;
 import com.clickcraft.demo.dto.client.ClientProfileUpdateRequest;
 import com.clickcraft.demo.models.ClientProfile;
 import com.clickcraft.demo.models.User;
-import com.clickcraft.demo.models.enums.ELocations;
 import com.clickcraft.demo.repository.ClientProfileRepository;
 import com.clickcraft.demo.security.repository.UserRepository;
 import com.clickcraft.demo.service.ClientProfileService;
+import com.clickcraft.demo.service.converter.ClientProfileConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -17,6 +17,7 @@ import org.springframework.transaction.annotation.Transactional;
 public class ClientProfileServiceImpl implements ClientProfileService {
 
     private final ClientProfileRepository clientProfileRepository;
+    private final ClientProfileConverter clientProfileConverter;
     private final UserRepository userRepository;
 
     @Override
@@ -31,17 +32,7 @@ public class ClientProfileServiceImpl implements ClientProfileService {
 
     @Transactional
     public void updateClientProfile(ClientProfile clientProfile, ClientProfileUpdateRequest clientProfileUpdateRequest) {
-        clientProfile.setFirstName(clientProfileUpdateRequest.getFirstName());
-        clientProfile.setLastName(clientProfileUpdateRequest.getLastName());
-        clientProfile.setContactPhone(clientProfileUpdateRequest.getContactPhone());
-        clientProfile.setLocation(ELocations.valueOf(clientProfileUpdateRequest.getLocation()));
-        clientProfile.setCompanyName(clientProfileUpdateRequest.getCompanyName());
-        clientProfile.setCompanyIndustry(clientProfileUpdateRequest.getCompanyIndustry());
-        clientProfile.setCompanySize(clientProfileUpdateRequest.getCompanySize());
-        clientProfile.setCompanyLocation(clientProfileUpdateRequest.getCompanyLocation());
-        clientProfile.setWebsite(clientProfileUpdateRequest.getWebsite());
-        clientProfile.setInstagram(clientProfileUpdateRequest.getInstagram());
-        clientProfile.setLinkedin(clientProfileUpdateRequest.getLinkedin());
+        clientProfileConverter.updateClientProfile(clientProfile, clientProfileUpdateRequest);
     }
 
     @Override
