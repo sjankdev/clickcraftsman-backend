9e13ba611b874270895857f17bbd49b031ad68cd
sjankdev <stefanjanko.dev@gmail.com>
2024-07-16 16:38:14 +0200

    Refactor JobController for Improved Error Handling and Functionality

--- src/main/java/com/clickcraft/demo/controllers/ClientController.java
+++ src/main/java/com/clickcraft/demo/controllers/ClientController.java
@@ -13,7 +13,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
--- src/main/java/com/clickcraft/demo/controllers/FreelancerController.java
+++ src/main/java/com/clickcraft/demo/controllers/FreelancerController.java
@@ -12,7 +12,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
